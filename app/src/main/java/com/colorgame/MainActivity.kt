73b50c5879ce66e8bd7cf8e2339c07b1c2ccf1a0
Daniil Kobitskoy <daniilkobitskoydaniil@mail.ru>
2023-04-21 19:51:18 +0300

    функция подгрузки иконок

--- app/src/main/java/com/colorgame/MainActivity.kt
+++ app/src/main/java/com/colorgame/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.colorgame
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+
+    fun loadImage(imageUrls: List<String>, imageViews: List<ImageView>) {
+        for (i in 0 until imageUrls.size.coerceAtMost(imageViews.size)) {
+            Picasso.get()
+                .load(imageUrls[i])
+                .into(imageViews[i])
+        }
+    }}
